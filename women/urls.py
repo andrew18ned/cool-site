2cc48b86bca8ac4aa51b14cf0fcae91f37929d80
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-03 21:20:30 +0300

    added static, style-css, url jinja

--- women/urls.py
+++ women/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
-    path('cats/<int:catid>/', category),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
 ]
--- women/views.py
+++ women/views.py
@@ -3,31 +3,32 @@ from django.shortcuts import redirect, render
 from .models import *
 
 
-menu = ['Про сайт', 'Добавити статтю', 'Зворотній зв*язок', 'Увійти']
+menu = [{'title' : 'Про сайт', 'url_name' : 'about'}, 
+        {'title' : 'Добавити статтю', 'url_name' : 'add_page'}, 
+        {'title' : 'Зворотній звязок', 'url_name' : 'contact'}, 
+        {'title' : 'Увійти', 'url_name' : 'login'},] 
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts':posts, 'menu':menu, 'title':'Головна'})
+    context = {
+        'posts':posts, 
+        'menu':menu, 
+        'title':'Головна',
+        }
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
     return render(request, 'women/about.html', {'menu':menu, 'title':'Про сайт'})
 
+def addpage(request):
+    return HttpResponse('Додавання статті')
 
-def category(request, catid):
-    if request.GET:
-        print(request.GET)
-    if catid == 10:
-        return redirect('home', permanent=True)
-    catid += 1
+def contact(request):
+    return HttpResponse('Зворотній звязок')
 
-    return HttpResponse(f'<h1>Сторінка категорій</h1><p>{catid}</p><br><a href="/cats/{catid}/">перейти на наступну сторінку</a><br><a href="/">перейти на головну</a>')
-
-
-def archive(request, year):
-    return HttpResponse(f'<center><h1 style="color:red">Сторінка містить архів по роках</h1><p>{year}</p><br><a href="/">перейти на головну</a></center>')
-
-
+def login(request):
+    return HttpResponse('Авторизація')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1 style="color:darkblue">Сторінка не знайдена :(</h1><br><a href="/">перейти на головну</a>')
