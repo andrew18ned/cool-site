30c4936c004f383621c81d9ef79926119bac9c88
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-04 11:52:02 +0300

    added upgrade admin panel(fields, filter, show info)

--- women/models.py
+++ women/models.py
@@ -1,14 +1,15 @@
+from tabnanny import verbose
 from django.urls import reverse
 from django.db import models
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name='Заговолок')
+    content = models.TextField(blank=True, verbose_name='Текст статті')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата створення')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Час змінення')
+    is_published = models.BooleanField(default=True, verbose_name='Публікація')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категорії')
 
     def __str__(self):
         return self.title
@@ -16,11 +17,21 @@ class Women(models.Model):
     def get_ablosute_url(self):
         return reverse('post', kwargs={'post_id':self.pk})
 
+    class Meta:
+        verbose_name = 'Відомі жінки'
+        verbose_name_plural = 'Відомі жінки'
+        ordering = ['time_create', 'title']
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категорія')
 
     def __str__(self):
         return self.name
 
     def get_ablosute_url(self):
         return reverse('category', kwargs={'cat_id':self.pk})
+
+    class Meta:
+        verbose_name = 'Категорію'
+        verbose_name_plural = 'Категорії'
+        ordering = ['id']
