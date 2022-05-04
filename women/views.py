d41bdb7d093230316b4f77e67bed48f61c0fd05c
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-04 11:18:55 +0300

    fix error, and added cat+date in everyone post

--- women/views.py
+++ women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect, render
 from .models import *
 
@@ -14,8 +14,8 @@ def index(request):
     context = {
         'posts':posts, 
         'cats' : cats,
-        'menu':menu, 
-        'title':'Головна',
+        'menu' : menu, 
+        'title' : 'Головна',
         'cat_selected' : 0,
         }
     return render(request, 'women/index.html', context=context)
@@ -40,4 +40,16 @@ def show_post(request, post_id):
     return HttpResponse(f'Відображення статті із id = {post_id}')
 
 def show_category(request, cat_id):
-    return HttpResponse(f'Відображення категорій із id = {cat_id}')
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) == 0:
+        raise Http404()
+        
+    context = {
+        'posts':posts, 
+        'cats' : cats,
+        'menu' : menu, 
+        'title' : 'Відображення по рубриках',
+        'cat_selected' : cat_id,
+        }
+    return render(request, 'women/index.html', context=context)
