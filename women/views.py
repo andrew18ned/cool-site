ba9e06fffecc029dfc4026bfea5cfb17e4a6f04c
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-04 18:13:44 +0300

    create template tags and add to base file html

--- women/views.py
+++ women/views.py
@@ -10,10 +10,8 @@ menu = [{'title' : 'Про сайт', 'url_name' : 'about'},
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {
         'posts':posts, 
-        'cats' : cats,
         'menu' : menu, 
         'title' : 'Головна',
         'cat_selected' : 0,
@@ -41,13 +39,11 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
     if len(posts) == 0:
         raise Http404()
         
     context = {
         'posts':posts, 
-        'cats' : cats,
         'menu' : menu, 
         'title' : 'Відображення по рубриках',
         'cat_selected' : cat_id,
