db8315f806250323b5e33330ad33b3d1fe0af43a
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-08 19:39:21 +0300

    edit admin panel for self

--- women/admin.py
+++ women/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Category, Women
 
 
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {'slug':('title',)}
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'is_published', 'get_html_photo', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = 'Мінатюра'
 
 
 
@@ -22,3 +32,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Women, WomenAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.site_title = 'Адмін-панель сайта про жінок'
+admin.site.site_header = 'Адмін-панель сайта про жінок 2'
